5aea5d0ea75d8f76f2437c11eb419c74f7a672ee
abrazax56 <ahmadbenirusli@gmail.com>
2024-03-01 06:24:30 +0700

    new commit

--- src/components/elements/Button.jsx
+++ src/components/elements/Button.jsx
@@ -2,7 +2,7 @@ export default ({children, isdisable, opsional}) => {
   return (
     <button
       type="submit"
-      className={`flex justify-center items-center rounded bg-blue-500 text-white w-full px-3 py-2 font-medium text-sm mt-3 cursor-pointer text-center ${opsional}`}
+      className={`flex justify-center items-center rounded bg-blue-500 text-white w-full px-3 py-2 font-medium text-sm mt-5 cursor-pointer text-center ${opsional}`}
       disabled={isdisable}>{children}</button>
   )
 }
