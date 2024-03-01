69809fa56f5415c71538e907bb19d541ee76ce0c
abrazax56 <ahmadbenirusli@gmail.com>
2024-03-01 08:32:12 +0700

    new commit

--- src/components/layouts/FormSignup.jsx
+++ src/components/layouts/FormSignup.jsx
@@ -21,9 +21,9 @@ export default () => {
     document.addEventListener('invalid', (() => {
       return (e) => {
         e.preventDefault();
-        if (_(".username").value === '') {
+        if (_(".username").value === '' && _('.username').value.length < 4) {
           _(".username").focus();
-        } else if (_(".password").value === '') {
+        } else if (_(".password").value === '' && _('.password').value.length < 4) {
           _(".password").focus();
         } else {
           _(".confirm").focus();
